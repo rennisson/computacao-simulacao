778a9289307cb534dba06d7404d4768c47ee3e98
rennisson <rennisson.alves@gmail.com>
2023-04-24 17:44:32 -0300

    performance melhorada

--- EP2/13687175_EP2.py
+++ EP2/13687175_EP2.py
@@ -11,110 +11,94 @@ B = 0.47950399848
 def main():
     np.random.seed(SEED)
 
-    print('CRUDE')
-    print('{0:^10} | {1:^25}'.format('Sample',
-                                     f'Estimativa'))
-    print('-'*35)
-    # for i in range(1, 3):
-    #     crude = Crude(13465688)
-    #     print('{0:^10} | {1:^25}'.format(f'{crude.n}',
+    # print('CRUDE')
+    # print('{0:^10} | {1:^25}'.format('Sample',
+    #                                  f'Estimativa'))
+    # print('-'*35)
+    # for i in range(1, 10):
+    #     n = 13465688
+    #     crude = Crude(n)
+    #     print('{0:^10} | {1:^25}'.format(f'{n}',
     #                                     f'{crude.estimativa()}'))
-
-    print('\n \n')
-    print('Hit or Miss')
-    print('{0:^10} | {1:^25}'.format('Sample',
-                                     f'Estimativa'))
-    print('-' * 35)
-    for i in range(1, 10):
-        hom = HitOrMiss(10128002)
-        print('{0:^10} | {1:^25}'.format(f'{hom.n}',
-                                         f'{hom.estimativa()}'))
-
-    print('\n \n')
-    print('IMPORTANCE SAMPLING')
-    print('{0:^10} | {1:^25}'.format('Sample',
-                                     f'Estimativa'))
-    print('-' * 35)
-    for i in range(1, 3):
-        imp_sample = ImportanceSampling(1000 * i)
-        print('{0:^10} | {1:^25}'.format(f'{imp_sample.n}',
-                                         f'{imp_sample.estimativa()}'))
+    #
+    # print('\n \n')
+    # print('Hit or Miss')
+    # print('{0:^10} | {1:^25}'.format('Sample',
+    #                                  f'Estimativa'))
+    # print('-' * 35)
+    # for i in range(1, 10):
+    #     n = 10128002
+    #     hom = HitOrMiss(n)
+    #     print('{0:^10} | {1:^25}'.format(f'{n}',
+    #                                      f'{hom.estimativa()}'))
+    #
+    # print('\n \n')
+    # print('IMPORTANCE SAMPLING')
+    # print('{0:^10} | {1:^25}'.format('Sample',
+    #                                  f'Estimativa'))
+    # print('-' * 35)
+    # n = 65281716
+    # for i in range(1, 10):
+    #     imp_sample = ImportanceSampling(n)
+    #     print('{0:^10} | {1:^25}'.format(f'{n}',
+    #                                      f'{imp_sample.estimativa()}'))
 
     print('\n \n')
     print('CONTROL VARIATE')
     print('{0:^10} | {1:^25}'.format('Sample',
                                      f'Estimativa'))
     print('-' * 35)
-    for i in range(1, 3):
-        control = ControlVariate(3059462)
-        print('{0:^10} | {1:^25}'.format(f'{control.n}',
+    for i in range(1, 10):
+        n = 3059462
+        control = ControlVariate(n)
+        print('{0:^10} | {1:^25}'.format(f'{n}',
                                          f'{control.estimativa()}'))
 
 
 class Crude:
     def __init__(self, n):
-        self.n = n
         # Choose randomly points in [0,1) using Uniform distribution
         points = np.random.uniform(0, 1, n)
-        self.soma = self.experimento(points)
+        f_values = np.exp(-A * points) * np.cos(B * points)
 
-    def experimento(self, points):
-        n = len(points)
-        soma = 0
-        for i in range(n):
-            soma += math.exp(-A*points[i]) * math.cos(B*points[i])
-
-        return soma / n
+        self.gamma_hat = f_values.sum() / n
 
     def estimativa(self):
-        return self.soma
+        return self.gamma_hat
 
 
 # VERIFICAR SE ESTÁ CERTO
 class HitOrMiss:
     def __init__(self, n):
-        self.n = n
         self.xPoints = np.random.uniform(0, 1, n)
         self.yPoints = np.random.uniform(0, 1, n)
-        self.soma = self.experimento()
-
-    def experimento(self):
         # Lista que vai armazenar os valores da função f(x)
-        f_values = np.zeros(self.n)  # inicializada com zeros
-        for i in range(len(self.n)):
-            f_values[i] = math.exp(-A * self.xPoints[i]) * math.cos(B * self.xPoints[i])
+        f_values = np.exp(-A * self.xPoints) * np.cos(B * self.xPoints)
 
         # retorna um array com TRUE se y < f_values. FALSE, caso contrario.
         h = (self.yPoints <= f_values)
 
-        soma = h.sum()
-
-        return soma / self.n
+        # h.sum() retorna a soma de todos os valores do array
+        self.gamma_hat = h.sum() / n
 
     def estimativa(self):
-        return self.soma
+        return self.gamma_hat
 
 
 class ImportanceSampling:
     def __init__(self, n):
-        self.n = n
         # g(x) = Beta Distribuition(alpha, beta)
         self.alpha = 0.35
         self.beta = 1
-        points = np.random.beta(self.alpha, self.beta, self.n)
+        points = np.random.beta(self.alpha, self.beta, n)  # geradora de pontos da função beta
+        f = np.exp(-A * points) * np.cos(B * points)  # guarda todos os valores da funcao f(x)
+        beta_values = beta.pdf(points, self.alpha, self.beta)  # guarda todos os valores da funcao beta(alpha, beta)
 
-        self.soma = self.experimento(points)
-
-    def experimento(self, points):
-        n = len(points)
-        soma = 0
-        for i in range(n):
-            soma += (np.exp(-A * points[i]) * np.cos(B * points[i])) / beta.pdf(points[i], self.alpha, self.beta)
-
-        return soma / self.n
+        # (f / beta_values).sum() retorna um array com a soma de todas as divisoes feitas entre 'f' e 'beta_values'
+        self.gamma_hat = (f / beta_values).sum() / n  # estimativa da importance sample
 
     def estimativa(self):
-        return self.soma
+        return self.gamma_hat
 
     def grafico(self):
         x = np.linspace(0, 1, 10000)
@@ -132,26 +116,25 @@ class ImportanceSampling:
         plt.show()
 
 
+# CONSERTAR OS VALORES DA SOMA
 class ControlVariate:
     def __init__(self, n):
-        self.n = n
         # phi(x) = -0.39727x + 1
         self.integration_phi = 0.801365  # integral de phi no intervalo (0,1)
         points = np.random.uniform(0, 1, n)
-        self.soma = self.experimento(points)
+        f_values = (np.exp(-A * points) * np.cos(B * points))
+        varphi_values = np.array((-0.39727 * points) + 1)
+        result = f_values.sum() - varphi_values.sum()
 
-    def experimento(self, points):
-        n = len(points)
-        soma = 0
-        for i in range(n):
-            soma += (np.exp(-A * points[i]) * np.cos(B * points[i])) \
-                    - (-0.39727*points[i] + 1) \
-                    + self.integration_phi
-
-        return soma / self.n
+        # soma = 0
+        # for i in range(n):
+        #     soma += (np.exp(-A * points[i]) * np.cos(B * points[i])) \
+        #             - (-0.39727 * points[i] + 1) \
+        #             + self.integration_phi
+        self.gamma_hat = (result.sum() + self.integration_phi) / n
 
     def estimativa(self):
-        return self.soma
+        return self.gamma_hat
 
 
 if __name__ == '__main__':
