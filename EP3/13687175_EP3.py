136d0b58e4c04437a6350caabd54ea9d753d3178
rennisson <rennisson.alves@gmail.com>
2023-05-05 00:38:39 -0300

    ep3, funcoes ok

--- EP3/13687175_EP3.py
+++ EP3/13687175_EP3.py
@@ -1,15 +1,12 @@
 import time
 import numpy as np
-from scipy.stats import beta
-from scipy.stats import qmc
+from scipy.stats import qmc, beta, uniform
 
 SEED = 13687175
 A = 0.386617636
 B = 0.47950399848
 
 def main():
-    np.random.seed(SEED)
-
     print('\n \n')
     print('CRUDE')
     print('{0:^10} | {1:^25} | {2:^25}'.format('Sample',
@@ -18,7 +15,7 @@ def main():
     print('-'*35)
     for i in range(1, 10):
         #n = 13465688
-        crude = Crude(10000*i)
+        crude = Crude(100*i)
         print('{0:^10} | {1:^25} | {2:^25}'.format(f'{1000*i}',
                                          f'{crude.estimativa()}',
                                          f'{crude.time}'))
@@ -31,32 +28,37 @@ def main():
     print('-' * 35)
     for i in range(1, 10):
         #n = 10128002
-        hom = HitOrMiss(1000*i)
-        print('{0:^10} | {1:^25} | {2:^25}'.format(f'{1000*i}',
+        n = 1000*i
+        hom = HitOrMiss(n)
+        print('{0:^10} | {1:^25} | {2:^25}'.format(f'{n}',
                                                    f'{hom.estimativa()}',
                                                    f'{hom.time}'))
-    #
-    # print('\n \n')
-    # print('IMPORTANCE SAMPLING')
-    # print('{0:^10} | {1:^25}'.format('Sample',
-    #                                  f'Estimativa'))
-    # print('-' * 35)
-    # n = 65281716
-    # for i in range(1, 10):
-    #     imp_sample = ImportanceSampling(n)
-    #     print('{0:^10} | {1:^25}'.format(f'{n}',
-    #                                      f'{imp_sample.estimativa()}'))
-    #
-    # print('\n \n')
-    # print('CONTROL VARIATE')
-    # print('{0:^10} | {1:^25}'.format('Sample',
-    #                                  f'Estimativa'))
-    # print('-' * 35)
-    # for i in range(1, 10):
-    #     n = 3059462
-    #     control = ControlVariate(n)
-    #     print('{0:^10} | {1:^25}'.format(f'{n}',
-    #                                      f'{control.estimativa()}'))
+
+    print('\n \n')
+    print('IMPORTANCE SAMPLING')
+    print('{0:^10} | {1:^25} | {2:^25}'.format(f'Sample',
+                                               f'Estimativa',
+                                               f'Tempo'))
+    print('-' * 35)
+    for i in range(1, 10):
+        n = 1000*i
+        imp_sample = ImportanceSampling(n)
+        print('{0:^10} | {1:^25} | {2:^25}'.format(f'{n}',
+                                                   f'{imp_sample.estimativa()}',
+                                                   f'{imp_sample.time}'))
+
+    print('\n \n')
+    print('CONTROL VARIATE')
+    print('{0:^10} | {1:^25}'.format('Sample',
+                                     f'Estimativa'))
+    print('-' * 35)
+    for i in range(1, 10):
+        #n = 3059462
+        n = 10*i
+        control = ControlVariate(n)
+        print('{0:^10} | {1:^25} | {2:^25}'.format(f'{n}',
+                                                   f'{control.estimativa()}',
+                                                   f'{control.time}'))
 
 
 class Crude:
@@ -77,9 +79,11 @@ class Crude:
 class HitOrMiss:
     def __init__(self, n):
         start_time = time.time()
-        points = halton(n, d=2)
+        points = halton(n, d=2).T
+        points = np.reshape(points, (2, n))
+
         # Lista que vai armazenar os valores da função f(x)
-        f_values = np.exp(-A * points[0]) * np.cos(B * points[1])
+        f_values = (np.exp(-A * points[0]) * np.cos(B * points[0]))
         # retorna um array com TRUE se y < f_values. FALSE, caso contrario.
         h = (np.array(points[1]) <= f_values)
 
@@ -93,15 +97,17 @@ class HitOrMiss:
 
 class ImportanceSampling:
     def __init__(self, n):
+        start_time = time.time()
         # g(x) = Beta Distribuition(alpha, beta)
         self.alpha = 0.35
         self.beta = 1
-        points = np.random.beta(self.alpha, self.beta, n)  # geradora de pontos da função beta
-        f_values = np.exp(-A * points) * np.cos(B * points)  # guarda todos os valores da funcao f(x)
+        points = halton_beta(n, self.alpha, self.beta, d=1).T  # geradora de pontos
+        f_values = (np.exp(-A * points) * np.cos(B * points))  # guarda todos os valores da funcao f(x)
         beta_values = beta.pdf(points, self.alpha, self.beta)  # guarda todos os valores da funcao beta(alpha, beta)
 
         # (f / beta_values).sum() retorna um array com a soma de todas as divisoes feitas entre 'f' e 'beta_values'
         self.gamma_hat = (f_values / beta_values).sum() / n  # estimativa da importance sample
+        self.time = time.time() - start_time
 
     def estimativa(self):
         return self.gamma_hat
@@ -110,21 +116,40 @@ class ImportanceSampling:
 # CONSERTAR OS VALORES DA SOMA
 class ControlVariate:
     def __init__(self, n):
+        start_time = time.time()
         # phi(x) = -0.39727x + 1
         self.integration_phi = 0.801365  # integral de phi no intervalo (0,1)
-        points = np.random.uniform(0, 1, n)
+        points = halton(n, d=1).T
         f_values = (np.exp(-A * points) * np.cos(B * points))  # valores de f(x)
         phi_values = np.array(-0.39727 * points + 1)  # valores da função que aproxima f(x)
 
         self.gamma_hat = (f_values - phi_values + self.integration_phi).sum() / n
+        self.time = time.time() - start_time
 
     def estimativa(self):
         return self.gamma_hat
 
 
-def halton(n, d=1):
-    sampler = qmc.Halton(d, scramble=True)
-    return np.array(sampler.random(n, workers=-1).T)
+def sobol(n, d=1):
+    sampler = qmc.Sobol(d, scramble=True, seed=SEED)
+    return sampler.random(n)
+
+
+def halton(n, d=1, seed=SEED):
+    sampler = qmc.Halton(d, scramble=True, seed=seed)
+    return sampler.random(n)
+
+
+def halton_beta(n, a, b, d=1):
+    sampler = qmc.Halton(d, scramble=True, seed=SEED)
+    x = sampler.random(n)
+    return beta.ppf(x, a, b)
+
+
+def halton_uniform(n, d=1):
+    sampler = qmc.Halton(d, scramble=True, seed=SEED)
+    x = sampler.random(n)
+    return uniform.ppf(x)
 
 
 if __name__ == '__main__':
