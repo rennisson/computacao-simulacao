5213f4b41e858c7662bfd0c28881f3e82912a461
rennisson <rennisson.alves@gmail.com>
2023-05-17 23:03:15 -0300

    EP4 - remis

--- EP4/13687175_EP4.py
+++ EP4/13687175_EP4.py
@@ -1,26 +1,31 @@
-import time
 import numpy as np
-import numpy.random
 from scipy import stats
 
 SEED = 13687175
-A = 0.386617636
-B = 0.47950399848
+N = 10000  # number of points
+K = 1000  # number of cuts
+V = 10  # cut we are interested in
 
 
 def main():
-    numpy.random.seed(13687175)
-    pvals = np.random.random(3)
-    print(f'pvals: {pvals}')
-    x = np.random.multinomial(10, pvals, 1)
-    y = np.random.multinomial(10, pvals, 1)
-    print(f'x: {x}, y: {y}')
-    theta = stats.dirichlet.rvs(x[0], size=1)
-    print(f'theta: {theta}')
-    posterior_function = posterior_density_function(theta, x, y)
-    print(f'posterior function: {posterior_function}')
-    sup_f = max(posterior_function[0])
-    print(sup_f)
+    np.random.seed(SEED)
+    pvals = np.random.random(3)  # pvals to use in multinomial generator
+    x = np.random.multinomial(10, pvals)
+    y = np.random.multinomial(10, pvals)
+    alpha = x + y
+    theta = stats.dirichlet.rvs(alpha, size=N)  # generates triples of theta
+    theta_density = stats.dirichlet.pdf(theta.T, alpha)  # gets dirichlet density from 'theta'
+    theta_density.sort()
+
+    cutoff_set = np.split(theta_density, K)  # generates 'K' cuts in 'theta_density'
+    soma = 0
+    for i in range(K):
+        if max(cutoff_set[i] >= V):
+            break
+        soma += cutoff_set[i].size
+
+    result = soma / N
+    print(f'Result: {result}')
 
 
 def posterior_density_function(theta, x, y):
