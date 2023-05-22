590476aa9662d1b32100d6da7c8e43f2a52d07b9
rennisson <rennisson.alves@gmail.com>
2023-05-22 13:28:11 -0300

    resolvido

--- EP4/EP_04_13685534.py
+++ EP4/EP_04_13685534.py
@@ -2,28 +2,6 @@ import sys
 import numpy as np
 import scipy.stats as stats
 
-<<<<<<< HEAD
-# Definições importantes 
-N = 10000000
-x = np.array([1, 2, 3])
-y = np.array([5, 2, 2])
-alpha = alpha2 = x+y
-k = 100000
-v = 10
-print(alpha)
-
-# Gerar valores das "tríades" teta, f(teta) e o maximo (v_k)
-teta = stats.dirichlet.rvs(alpha, size=N)
-f = stats.dirichlet.pdf(teta.T, alpha)
-f.sort()
-
-f = np.split(f, k)
-soma = 0
-for c in range(0, k):
-    soma += f[c].size
-    if max(f[c]) >= v:
-        break
-=======
 def main():
     # Definições importantes 
     np.random.seed(13685534)
@@ -40,7 +18,6 @@ def main():
         if(v == -1): break
         else: calcula_U(val_f, k, N, v)
     
->>>>>>> ab6691fc52d6b318202acf36fd0155e607a6a7fd
     
 
 def gera_valores(alpha, k, N):
