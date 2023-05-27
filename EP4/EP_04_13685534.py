d836098884f92aab8cc90c2ab199536896976a14
rennisson <rennisson.alves@gmail.com>
2023-05-27 16:51:11 -0300

    versao final

--- EP4/EP_04_13685534.py
+++ EP4/EP_04_13685534.py
@@ -21,8 +21,10 @@ def main():
         if v == -1:
             break
         else:
-            print(calcula_U_teste(val_f, k, N, v))
-
+            precisao, massa, tempo = calcula_U_teste(val_f, k, N, v)
+            print(f'U(v): {massa:0.4f}')
+            print(f'Precisão: {precisao}%')
+            print(f'Tempo: {tempo:0.4f}\n')
 
 def gera_valores(alpha, k, N):
     # Gerar valores das "tríades" teta, f(teta) e o maximo (v_k)
@@ -51,12 +53,15 @@ def calcula_U(f, k, N, v, mode):
 def calcula_U_teste(f, k, N, v):
     ref = calcula_U(f, k, 1000000, v, 0)
     arr = np.zeros(100)
+    tempo = np.zeros(100)
     for c in range(0, 100):
+        start_time = time.time()
         arr[c] = calcula_U(f, k, N, v, 0)
+        tempo[c] = time.time() - start_time
     t_1 = arr <= ref * 1.0005
     t_2 = arr >= ref * 0.9995
     t = t_1 & t_2
-    return t.sum()
+    return t.sum(), arr.mean(), tempo.mean()
 
 
 if __name__ == '__main__':
