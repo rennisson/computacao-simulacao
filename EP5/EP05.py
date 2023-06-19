d7586884cc6460e003ec7cdb7ff2943d8f0dd2bf
rennisson <rennisson.alves@gmail.com>
2023-06-18 23:05:55 -0300

    finalizando

--- EP5/EP05.py
+++ EP5/EP05.py
@@ -17,7 +17,7 @@ Testes foram feitos com a seed definida.
 
 def main():
     # Definições importantes 
-    np.random.seed(13685534)
+    np.random.seed(13687175)
     N = int(input("Digite o valor para N: "))
     k = int(input("Digite o valor para K: "))
 
@@ -28,7 +28,8 @@ def main():
 
     # Gerar os valores usados para os cálculos de acordo com a Dirichlet
     val_f = gera_valores(alpha, k, N)
-    ref_f = gera_valores(alpha, 1000000, 10000000)
+    print("Entrou aqui!")
+    ref_f = gera_valores(alpha, 10000, 1000000)
 
     print(f"Valor máximo global de f: {np.amax(val_f)}\n")
 
@@ -42,47 +43,49 @@ def main():
             print(f'Precisão: {precisao}%')
             print(f'Tempo: {tempo:0.4f}\n')
 
+
 # Gera valores já repartidos de acordo com a Dirichlet
 def gera_valores(theta, k, N): 
     f = np.zeros((N, 3))
     theta_1 = stats.multivariate_normal.rvs((0.3, 0.3, 0.4))            # Primeiro Theta arbitrário
 
     while True:                                                        
-        if(theta_1[0] < 0 or theta_1[1] < 0 or theta_1[2] < 0): 
+        if theta_1[0] < 0 or theta_1[1] < 0 or theta_1[2] < 0:
             theta_1 = stats.multivariate_normal.rvs((0.3, 0.3, 0.4))
         else: 
             theta_1 = theta_1 / theta_1.sum()
             break 
 
-    for c in range(0, 200):                                             # Aquecimento da cadeira
+    for c in range(0, 200):                                             # Aquecimento da cadeia
         theta_2 = stats.multivariate_normal.rvs(theta_1, cov=0.06)
-        if(theta_2[0] < 0 or theta_2[1] < 0 or theta_2[2] < 0):         # Rejeitar se tiver fora do domínio
+        if theta_2[0] < 0 or theta_2[1] < 0 or theta_2[2] < 0:         # Rejeitar se tiver fora do domínio
             alpha = 0
         else: 
             theta_2 = theta_2 / theta_2.sum()
             alpha = stats.dirichlet.pdf(theta_2, theta) / stats.dirichlet.pdf(theta_1, theta)
-        if(alpha >= 1):                                                 # Aceitar o rejeitar de acordo com o critério do algorítmo
+        if alpha >= 1:                                                 # Aceitar ou rejeitar de acordo com o critério do algorítmo
             f[c] = theta_2
         else:
             rand = random.uniform(0, 1)
-            if(rand < alpha):
+            if rand < alpha:
                 f[c] = theta_2
             else: 
                 f[c] = theta_1
+
     theta_1 = f[199]
 
     for c in range(0, N):                                               # Parte principal de geração de f
         theta_2 = stats.multivariate_normal.rvs(theta_1, cov=0.06)
-        if(theta_2[0] < 0 or theta_2[1] < 0 or theta_2[2] < 0):         # Rejeitar se tiver fora do domínio
+        if theta_2[0] < 0 or theta_2[1] < 0 or theta_2[2] < 0:         # Rejeitar se tiver fora do domínio
             alpha = 0
         else: 
             theta_2 = theta_2 / theta_2.sum()
             alpha = stats.dirichlet.pdf(theta_2, theta) / stats.dirichlet.pdf(theta_1, theta)
-        if(alpha >= 1):                                                 # Aceitar o rejeitar de acordo com o critério do algorítmo
+        if alpha >= 1:                                                 # Aceitar o rejeitar de acordo com o critério do algorítmo
             f[c] = theta_2
         else:
             rand = random.uniform(0, 1)
-            if(rand < alpha):
+            if rand < alpha:
                 f[c] = theta_2
             else: 
                 f[c] = theta_1
@@ -91,6 +94,7 @@ def gera_valores(theta, k, N):
     g.sort()
     g = np.split(g, k)
     return g
+
 
 # Calcula o valor de U(v)
 def calcula_U(f, k, N, v, mode):
@@ -107,15 +111,18 @@ def calcula_U(f, k, N, v, mode):
     else:
         return float(soma) / float(N)
 
+
 # Teste para estimação da precisão da função calcula_U
 def calcula_U_teste(f, k, N, v, ref_f):
-    ref = calcula_U(ref_f, 1000000, 10000000, v, 0)
+    ref = calcula_U(ref_f, 10000, 100000, v, 0)
+    print(ref)
     arr = np.zeros(100)
     tempo = np.zeros(100)
     for c in range(0, 100):
         start_time = time.time()
         arr[c] = calcula_U(f, k, N, v, 0)
         tempo[c] = time.time() - start_time
+
     t_1 = arr <= ref * 1.0005
     t_2 = arr >= ref * 0.9995
     t = t_1 & t_2
